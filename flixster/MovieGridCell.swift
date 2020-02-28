30810a9a00fef4559562380e3d95f9f1f99a6e03
vinhvu <vinhvu224@gmail.com>
2020-02-27 19:08:13 -0800

    assignment 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Vinh Vu on 2/27/20.
+//  Copyright © 2020 Vinh Vu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
